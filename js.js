0ba41aedd9692080f87ca46a176256f7854096fe
trangpt1010 <phantrang194@gmail.com>
2020-09-24 09:09:17 +0900

    Update js.js

--- js.js
+++ js.js
@@ -32,7 +32,23 @@ $( document ).ready(function() {
     
    })
     
-   $('img').click(function()
+   $('img').on('touchstart', function()
+   {
+     if(anh1)
+    {
+      $("audio")[0].play();
+       $(this).attr("src","2.jpg");
+       anh1=false;
+    }
+    else
+    {
+      $(this).attr("src","1.jpg");
+      anh1=true;
+    }
+    
+   })
+    
+       $('img').on('touchend', function()
    {
      if(anh1)
     {
