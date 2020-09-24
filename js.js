c8b38e37e0c0169476988b569f38e59364900646
trangpt1010 <phantrang194@gmail.com>
2020-09-24 09:00:17 +0900

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,22 @@
 anh1=true;;
 $( document ).ready(function() {
-    $('img').click(function()
+    $('img').mouseup(function()
+   {
+     if(anh1)
+    {
+      $("audio")[0].play();
+       $(this).attr("src","2.jpg");
+       anh1=false;
+    }
+    else
+    {
+      $(this).attr("src","1.jpg");
+      anh1=true;
+    }
+    
+   })
+   
+   $('img').mousedown(function()
    {
      if(anh1)
     {
